62342b53642beb35c561bf4c1288b99ec6b56101
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-09-12 17:47:49 +0200

    Update ui

--- app/src/main/java/com/example/android/exploreromania/MustSeeFragment.java
+++ app/src/main/java/com/example/android/exploreromania/MustSeeFragment.java
@@ -22,9 +22,9 @@ public class MustSeeFragment extends SightFragment {
         Location bucharest = new Location(getString(R.string.bucharest_city_name), ro);
 
         sights.add(new Sight(getString(R.string.bran_castle_name), getString(R.string.bran_castle_desc), SightType.Castle, 4.5f, bran, R.drawable.bran_castle));
+        sights.add(new Sight(getString(R.string.trans_road_name), getString(R.string.trans_road_desc), SightType.Road, 4.5f, romania, R.drawable.transfagarasan));
         sights.add(new Sight(getString(R.string.salina_mine_name), getString(R.string.salina_mine_desc), SightType.Mine, 4.6f, turda, R.drawable.salina2));
         sights.add(new Sight(getString(R.string.parl_palace_name), getString(R.string.parl_palace_desc), SightType.Palace, 4.5f, bucharest, R.drawable.parliament));
-        sights.add(new Sight(getString(R.string.trans_road_name), getString(R.string.trans_road_desc), SightType.Road, 4.5f, romania, R.drawable.transfagarasan));
         sights.add(new Sight(getString(R.string.balea_lake_name), getString(R.string.balea_lake_desc), SightType.Lake, 4.8f, romania, R.drawable.balea_lac));
 
         return sights;
